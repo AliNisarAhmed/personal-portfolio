dc650774ea2eba667e2d394fde68773cbdec1bbf
AliNisarAhmed <ali.nisar87@gmail.com>
2019-03-11 23:44:01 +0500

    adding projects

---
+++ src/content/projects.js
@@ -0,0 +1,14 @@
+
+
+export default [
+  {
+    title: 'Zilch',
+    description: 'A 2-player Dice game built with React Hooks & Styled-Components',
+    skills: ['React', 'React Hooks', 'styled-components'],
+    image: '',
+    links: {
+      preview: '',
+      github: '',
+    }
+  }
+]
